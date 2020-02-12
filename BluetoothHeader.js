178887065e46e155060eedf942f24817fb19fa2e
Dylan Gardner <>
2020-02-12 12:30:34 -0600

    Bluetooth

--- BluetoothHeader.js
+++ BluetoothHeader.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 
-import { BleManager, BleManagerOptions } from 'react-native-ble-plx';
+global.Buffer = global.Buffer || require('buffer').Buffer
+
+
+import { BleManager } from 'react-native-ble-plx';
 import MusicHeader from './MusicHeader';
 
 class BluetoothHeader extends Component {
     constructor(props) {
         super(props);
-        this.state = {  };
+        this.state = { device: null };
         this.manager = new BleManager();//new BleManagerOptions(restoreStateIdentifier, restoreStateFunction));
     }
 
@@ -27,21 +30,32 @@ class BluetoothHeader extends Component {
                 return
             }
     
-            // Check if it is a device you are looking for based on advertisement data
-            // or other criteria.
             if (device.name === 'Adafruit Bluefruit LE') {
-                
-                // Stop scanning as it's not necessary if you are scanning for one device.
                 this.manager.stopDeviceScan();
     
                 // Proceed with connection.
-                device.connect()
+                this.device = device
+                this.device.connect()
                     .then((device) => {
                         return device.discoverAllServicesAndCharacteristics()
                     })
                     .then((device) => {
                     // Do work on device with services and characteristics
-                        console.log(device)
+                        var obj = {music: 'Song'}
+                        let objJsonStr = JSON.stringify(obj);
+                        let objJsonB64 = Buffer.from(objJsonStr).toString("base64");
+                        device.writeCharacteristicWithResponseForService('6e400001-b5a3-f393-e0a9-e50e24dcca9e', '6e400002-b5a3-f393-e0a9-e50e24dcca9e', objJsonB64)
+                        .then((characteristic) => {
+                            console.log(characteristic)
+                            return 
+                        })
+                        device.monitorCharacteristicForService('6e400001-b5a3-f393-e0a9-e50e24dcca9e', '6e400003-b5a3-f393-e0a9-e50e24dcca9e', (error, characteristic) => {
+                            if (error) {
+                              this.error(error.message)
+                              return
+                            }
+                            console.log(Base64.decode(characteristic.value))
+                          })
                     })
                     .catch((error) => {
                         // Handle errors
@@ -56,6 +70,134 @@ class BluetoothHeader extends Component {
         return (
             <MusicHeader/>
         );
+    }
+}
+
+var Base64 = {
+
+    _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+
+    encode : function (input) {
+
+
+        var output = "";
+
+
+        var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+
+
+        var i = 0;
+
+        input = Base64._utf8_encode(input);
+        while (i < input.length) {
+            chr1 = input.charCodeAt(i++);
+            chr2 = input.charCodeAt(i++);
+            chr3 = input.charCodeAt(i++);
+
+            enc1 = chr1 >> 2;
+            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+            enc4 = chr3 & 63;
+            if (isNaN(chr2)) {
+                enc3 = enc4 = 64;
+            } else if (isNaN(chr3)) {
+                enc4 = 64;
+            }
+
+ 
+            output = output +
+            this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+            this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+
+        }
+        return output;
+    },
+
+ 
+
+
+    // public method for decoding
+
+
+    decode : function (input) {
+        var output = "";
+        var chr1, chr2, chr3;
+        var enc1, enc2, enc3, enc4;
+        var i = 0;
+        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+        while (i < input.length) {
+
+            enc1 = this._keyStr.indexOf(input.charAt(i++));
+            enc2 = this._keyStr.indexOf(input.charAt(i++));
+            enc3 = this._keyStr.indexOf(input.charAt(i++));
+            enc4 = this._keyStr.indexOf(input.charAt(i++));
+            chr1 = (enc1 << 2) | (enc2 >> 4);
+            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+            chr3 = ((enc3 & 3) << 6) | enc4;
+            output = output + String.fromCharCode(chr1);
+            if (enc3 != 64) {
+                output = output + String.fromCharCode(chr2);
+            }
+            if (enc4 != 64) {
+                output = output + String.fromCharCode(chr3);
+            }
+        }
+        output = Base64._utf8_decode(output);
+        return output;
+    },
+    // private method for UTF-8 encoding
+    _utf8_encode : function (string) {
+        string = string.replace(/\r\n/g,"\n");
+        var utftext = "";
+        for (var n = 0; n < string.length; n++) {
+            var c = string.charCodeAt(n);
+
+            if (c < 128) {
+                utftext += String.fromCharCode(c);
+            }
+            else if((c > 127) && (c < 2048)) {
+                utftext += String.fromCharCode((c >> 6) | 192);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+            else {
+                utftext += String.fromCharCode((c >> 12) | 224);
+                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+        }
+
+        return utftext;
+    },
+    // private method for UTF-8 decoding
+
+
+    _utf8_decode : function (utftext) {
+        var string = "";
+        var i = 0;
+        var c = c1 = c2 = 0;
+
+        while ( i < utftext.length ) {
+
+            c = utftext.charCodeAt(i);
+
+            if (c < 128) {
+                string += String.fromCharCode(c);
+                i++;
+            }
+            else if((c > 191) && (c < 224)) {
+                c2 = utftext.charCodeAt(i+1);
+                string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+                i += 2;
+            }
+            else {
+                c2 = utftext.charCodeAt(i+1);
+                c3 = utftext.charCodeAt(i+2);
+                string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+                i += 3;
+            }
+        }
+        return string;
     }
 }
 
